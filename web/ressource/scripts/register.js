0230cfe17040c09031cc4b0fa3716da69106adfd
TwetLeCarry <arthur.richelet@gmail.com>
2023-07-11 19:27:58 +0200

    modified database & added login and register

---
+++ web/ressource/scripts/register.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#login-submit').on('click', function() {
+      $('#response').html('');
+        var email = $('#email').val();
+        var password = $('#password').val();
+    })
+})
