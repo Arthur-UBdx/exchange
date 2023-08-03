9d05aaba3fe9b3ed408fc4bdbc52468285f34136
Hugo Dubois <duboish00@gmail.com>
2023-08-03 17:57:50 +0300

    Add prices of token

--- exchange/src/components/App.js
+++ exchange/src/components/App.js
@@ -1,9 +1,11 @@
 import Banner from './Banner'
+import BtcPrice from './Btc_price'
 
 function App() {
 	return (
 		<div>
 			<Banner/>
+			<BtcPrice/>
 		</div>
 	)
 }
---
+++ exchange/src/components/Btc_price.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const tokenSymbols = ["BTCUSDT", "ETHUSDT"];
+
+const TokenList = () => {
+  const [tokenPrices, setTokenPrices] = useState([]);
+
+  useEffect(() => {
+    const fetchTokenPrices = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.binance.com/api/v3/ticker/price"
+        );
+
+        const tokensWithTrueValue = response.data.filter(
+            (token) => tokenSymbols.indexOf(token.symbol) !== -1
+        );
+
+        const prices = tokensWithTrueValue.map((token) => token.price);
+
+        setTokenPrices(prices);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des prix des tokens :", error);
+      }
+    };
+
+    fetchTokenPrices();
+
+    const interval = setInterval(fetchTokenPrices, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h2>Liste des prix des tokens :</h2>
+      <ul>
+        {tokenPrices.map((price, index) => (
+          <li key={index}>{parseFloat(price).toFixed(2)} USDT</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TokenList;
