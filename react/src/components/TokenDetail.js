341ab11733624543a992bf4eb1a74e8463104f50
Hugo Dubois <duboish00@gmail.com>
2023-08-14 15:50:17 +0200

    fetch candles datas

--- react/src/components/TokenDetail.js
+++ react/src/components/TokenDetail.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const TokenDetail = () => {
   const { id } = useParams();
+  const interval = '15m';
+  const url = `https://api.binance.com/api/v3/klines?symbol=${id}&interval=${interval}`;
+  
+  const getCandles = async () => {
+    try {
+      const response = await axios.get(url);
+      const candles = response.data;
+	  console.log('Candles:', candles);
+    } catch (error) {
+      console.error('Error fetching candles:', error.message);
+    }
+  };
 
   return (
     <div>
-      <h2>Token Detail for {id}</h2>
+      <h1>Token Detail</h1>
+      <p>Token ID: {id}</p>
+      <button onClick={getCandles}>Get Candles</button>
     </div>
   );
 };
