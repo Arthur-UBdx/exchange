4a3ab45941f5d4d62b752f531f4793aa9291ec9d
Hugo Dubois <duboish00@gmail.com>
2023-08-16 18:50:09 +0200

    amélioration affichage charts

--- react/src/components/TokenDetail.js
+++ react/src/components/TokenDetail.js
@@ -5,41 +5,59 @@ import ReactApexChart from 'react-apexcharts';
 
 const TokenDetail = () => {
     const { id } = useParams();
-    const interval = '15m';
+    const [interval, setInterval] = useState('15m');
     const url = `https://api.binance.com/api/v3/klines?symbol=${id}&interval=${interval}`;
     
     const [candles, setCandles] = useState([]);
 
-    const getCandles = async () => {
-        try {
-            const response = await axios.get(url);
-            const candlesData = response.data.map(item => ({
-                x: new Date(item[0]).toISOString(),
-                y: [
-                    parseFloat(item[1]),
-                    parseFloat(item[2]),
-                    parseFloat(item[3]),
-                    parseFloat(item[4])
-                ]
-            }));
-            setCandles(candlesData);
-        } catch (error) {
-            console.error('Error fetching candles:', error.message);
-        }
-    };
-
     useEffect(() => {
+        const getCandles = async () => {
+            try {
+                const response = await axios.get(url);
+                const candlesData = response.data.map(item => ({
+                    x: new Date(item[0]).toISOString(),
+                    y: [
+                        parseFloat(item[1]),
+                        parseFloat(item[2]),
+                        parseFloat(item[3]),
+                        parseFloat(item[4])
+                    ]
+                }));
+                setCandles(candlesData);
+            } catch (error) {
+                console.error('Error fetching candles:', error.message);
+            }
+        };
+    
         getCandles();
-    });
+    }, [id, interval, url]);
 
     const options = {
         chart: {
             type: 'candlestick',
-            height: 350
+            height: 350,
+            toolbar: {
+                show: true,
+                tools: {
+                    download: false,
+                    selection: true,
+                    zoom: true,
+                    zoomin: true,
+                    zoomout: true,
+                    pan: true,
+                    reset: true,
+                },
+                autoSelected: 'zoom'
+            },
+            background: '#1a202c',
+            foreColor: '#A0AEC0'
         },
         title: {
             text: id,
-            align: 'left'
+            align: 'left',
+            style: {
+                color: '#A0AEC0'
+            }
         },
         xaxis: {
             type: 'datetime'
@@ -48,11 +66,31 @@ const TokenDetail = () => {
             tooltip: {
                 enabled: true
             }
+        },
+        theme: {
+            mode: 'dark'
+        },
+        plotOptions: {
+            candlestick: {
+                colors: {
+                    upward: '#2ecc71',
+                    downward: '#e74c3c'
+                }
+            }
+        },
+        grid: {
+            borderColor: '#4A5568'
         }
     };
 
     return (
-        <div>
+        <div style={{ background: "#2D3748", padding: "20px" }}>
+            <div>
+                <button onClick={() => setInterval('15m')}>15m</button>
+                <button onClick={() => setInterval('1h')}>1h</button>
+                <button onClick={() => setInterval('4h')}>4h</button>
+                <button onClick={() => setInterval('1d')}>1d</button>
+            </div>
             <ReactApexChart options={options} series={[{ data: candles }]} type="candlestick" height={350} />
         </div>
     );
