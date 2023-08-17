3ce6743d4d7f25f309491628932e9485ca56d09b
Hugo Dubois <duboish00@gmail.com>
2023-08-17 18:21:30 +0200

    Amelioration chart (css a fix)

--- react/src/components/TokenDetail.js
+++ react/src/components/TokenDetail.js
@@ -1,99 +1,72 @@
-/* eslint-disable no-implied-eval */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import ReactApexChart from 'react-apexcharts';
+import { createChart } from 'lightweight-charts';
+import styles from '../styles/TokenDetail.module.css';
 
 const TokenDetail = () => {
     const { id } = useParams();
+    const chartRef = useRef(null);
     const [interval, setInterval] = useState('15m');
     const [candles, setCandles] = useState([]);
+    const chartContainerRef = useRef(null);
+    const candlestickSeriesRef = useRef(null);
 
     useEffect(() => {
-        const fetchCandles = async () => {
-            const url = `https://api.binance.com/api/v3/klines?symbol=${id}&interval=${interval}&limit=100`;
+        const url = `https://api.binance.com/api/v3/klines?symbol=${id}&interval=${interval}`;
+        const getCandles = async () => {
             try {
                 const response = await axios.get(url);
                 const candlesData = response.data.map(item => ({
-                    x: new Date(item[0]).toISOString(),
-                    y: [
-                        parseFloat(item[1]),
-                        parseFloat(item[2]),
-                        parseFloat(item[3]),
-                        parseFloat(item[4])
-                    ]
+                    time: Math.floor(item[0] / 1000),
+                    open: parseFloat(item[1]),
+                    high: parseFloat(item[2]),
+                    low: parseFloat(item[3]),
+                    close: parseFloat(item[4])
                 }));
                 setCandles(candlesData);
             } catch (error) {
                 console.error('Error fetching candles:', error.message);
             }
         };
-
-        fetchCandles();
+        getCandles();
     }, [id, interval]);
 
-    const options = {
-        chart: {
-            type: 'candlestick',
-            height: 500,
-            toolbar: {
-                show: true,
-                tools: {
-                    download: true,
-                    selection: true,
-                    zoom: false,
-                    zoomin: true,
-                    zoomout: true,
-                    pan: true,
-                    reset: true,
-                },
-                autoSelected: 'zoom'
-            },
-            background: '#1a202c',
-            foreColor: '#A0AEC0'
-        },
-        title: {
-            text: id,
-            align: 'left',
-            style: {
-                color: '#A0AEC0'
-            }
-        },
-        xaxis: {
-            type: 'datetime'
-        },
-        yaxis: {
-            tooltip: {
-                enabled: true
-            }
-        },
-        theme: {
-            mode: 'dark'
-        },
-        plotOptions: {
-            candlestick: {
-                colors: {
-                    upward: '#2ecc71',
-                    downward: '#e74c3c'
-                }
-            }
-        },
-        grid: {
-            borderColor: '#4A5568'
+    useEffect(() => {
+        if (chartContainerRef.current && !chartRef.current) {
+            chartRef.current = createChart(chartContainerRef.current, { width: 800, height: 400 });
+            candlestickSeriesRef.current = chartRef.current.addCandlestickSeries();
         }
+
+        if (candles.length > 0 && candlestickSeriesRef.current) {
+            candlestickSeriesRef.current.setData(candles);
+        }
+
+        return () => {
+            if (chartRef.current) {
+                chartRef.current.remove();
+                chartRef.current = null;
+                candlestickSeriesRef.current = null;
+            }
+        };
+    }, [candles]);
+
+    const handleIntervalChange = (newInterval) => {
+        setInterval(newInterval);
     };
 
     return (
-        <div style={{ background: "#2D3748", padding: "20px" }}>
-            <div>
-                <button onClick={() => setInterval('15m')}>15m</button>
-                <button onClick={() => setInterval('1h')}>1h</button>
-                <button onClick={() => setInterval('4h')}>4h</button>
-                <button onClick={() => setInterval('1d')}>1d</button>
+        <div className={styles.tokenDetailContainer}>
+            <h1 className={styles.tokenDetailHeader}>{id}</h1>
+            <div className={styles.tokenDetailIntervalButtons}>
+                <button className={styles.tokenDetailIntervalButton} onClick={() => handleIntervalChange('15m')}>15m</button>
+                <button className={styles.tokenDetailIntervalButton} onClick={() => handleIntervalChange('1h')}>1h</button>
+                <button className={styles.tokenDetailIntervalButton} onClick={() => handleIntervalChange('4h')}>4h</button>
+                <button className={styles.tokenDetailIntervalButton} onClick={() => handleIntervalChange('1d')}>1d</button>
             </div>
-            <ReactApexChart options={options} series={[{ data: candles }]} type="candlestick" height={500} />
+            <div ref={chartContainerRef}></div>
         </div>
-    );
+    );    
 };
 
 export default TokenDetail;
